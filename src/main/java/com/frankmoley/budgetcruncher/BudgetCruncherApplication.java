7098422e98aca6298143c9eb07050f47a28f62c1
Frank P Moley III <frank@catchandrelease.com>
2022-11-13 12:37:49 -0600

    Initial project structure

---
+++ src/main/java/com/frankmoley/budgetcruncher/BudgetCruncherApplication.java
@@ -0,0 +1,13 @@
+package com.frankmoley.budgetcruncher;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BudgetCruncherApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(BudgetCruncherApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/frankmoley/budgetcruncher/BudgetCruncherApplicationTests.java
@@ -0,0 +1,13 @@
+package com.frankmoley.budgetcruncher;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BudgetCruncherApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
